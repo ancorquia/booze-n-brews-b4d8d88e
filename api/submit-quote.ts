dd483bf8f97fe68ddf32e57487230904e82950b0
ancorquia <149346914+ancorquia@users.noreply.github.com>
2025-06-08 23:44:40 -0600

    Update submit-quote.ts
    removed runtime: 'edge',
    for function to send.

--- api/submit-quote.ts
+++ api/submit-quote.ts
@@ -1,9 +1,5 @@
 import { Resend } from 'resend';
 
-export const config = {
-  runtime: 'edge',
-};
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(request: Request) {
